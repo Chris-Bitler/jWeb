5baeaadde2115abbe43bc314d9e6e65e3b064604
VoidWhisperer <voidwhisperer@voksul.net>
2015-05-04 09:01:21 -0400

    Add cryptography utility class
    Has methods for fast sha1 with or without salt, along with a method that
    can use other algorithms with or without salt - No MD5 as it has been
    deemed insecure.

---
+++ src/main/java/net/voksul/webserver/Crytography.java
@@ -0,0 +1,65 @@
+package net.voksul.webserver;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Crytography {
+    public static String sha1(String text)
+    {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hash = digest.digest(text.getBytes("UTF-8"));
+            return new String(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String sha1(String text, String salt)
+    {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            byte[] hash = digest.digest(text.getBytes("UTF-8"));
+            return new String(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String hash(String algorithm, String text)
+    {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            byte[] hash = digest.digest(text.getBytes("UTF-8"));
+            return new String(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String hash(String algorithm, String text, String salt)
+    {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            byte[] hash = digest.digest(text.getBytes("UTF-8"));
+            return new String(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //No MD5 is included as it has been proven insecure.
+}
